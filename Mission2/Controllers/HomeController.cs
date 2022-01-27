eafc9d22001c88b85887aad2b367dc5d958f5101
kaylajt-1 <kayla.telford@byu.edu>
2022-01-26 22:52:35 -0700

    Mission3 changes

---
+++ Mission2/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mission2.Models;
+
+namespace Mission2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GradeCalculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GradeCalculator(GradeCalculaterModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission2/Models/GradeCalculaterModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission2.Models
+{
+    public class GradeCalculaterModel
+    {
+        [Required]
+        [Range(0, 100)]
+        public float assignment { get; set; }
+
+        [Required]
+        //[Range(0, 100, ErrorMessage = "Value for must be between {0} and {100}.")]
+        public float groupProject { get; set; }
+
+        [Required]
+        //[Range(0, 100, ErrorMessage = "Value for must be between {0} and {100}.")]
+        public float quizzes { get; set; }
+
+        [Required]
+        //[Range(0, 100, ErrorMessage = "Value for must be between {0} and {100}.")]
+        public float exams { get; set; }
+
+        [Required]
+        //[Range(0, 100, ErrorMessage = "Value for must be between {0} and {100}.")]
+        public float intex { get; set; }
+
+    }
+}
